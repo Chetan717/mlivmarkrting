c6d44e7ae0b71744f98b6e05495b5e4ed057ff24
chetan717 <chaitanyachaudhari717@gmail.com>
2026-04-25 09:36:38 +0530

    removebg key

--- Firebase.js
+++ Firebase.js
@@ -2,17 +2,26 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage"; 
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAZ-HEbjGAy6gKPXxFcrdNwKypVk2NXv2A",
+//   authDomain: "mlmbooster.firebaseapp.com",
+//   projectId: "mlmbooster",
+//   storageBucket: "mlmbooster.firebasestorage.app",
+//   messagingSenderId: "649090963301",
+//   appId: "1:649090963301:web:83247d75d046e6fb46b38c",
+//   measurementId: "G-C3FTH3Q87N",
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyAZ-HEbjGAy6gKPXxFcrdNwKypVk2NXv2A",
-  authDomain: "mlmbooster.firebaseapp.com",
-  projectId: "mlmbooster",
-  storageBucket: "mlmbooster.firebasestorage.app",
-  messagingSenderId: "649090963301",
-  appId: "1:649090963301:web:83247d75d046e6fb46b38c",
-  measurementId: "G-C3FTH3Q87N",
+  apiKey: "AIzaSyB8i48UiJ9YJ1w9_e-cwthvbqB94eNLulc",
+  authDomain: "mlmbooster-a4887.firebaseapp.com",
+  projectId: "mlmbooster-a4887",
+  storageBucket: "mlmbooster-a4887.firebasestorage.app",
+  messagingSenderId: "535257539526",
+  appId: "1:535257539526:web:13cbba2f093fcf277088c7",
+  measurementId: "G-YB76LFZQ5X"
 };
 
 // Initialize Firebase
@@ -20,8 +29,8 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize services
 const db = getFirestore(app);
-const analytics = getAnalytics(app); 
+const analytics = getAnalytics(app);
 const storage = getStorage(app);
 
 // Export
-export { db, app, analytics,storage };
+export { db, app, analytics, storage };
